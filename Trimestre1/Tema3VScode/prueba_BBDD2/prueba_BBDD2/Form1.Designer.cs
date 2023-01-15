99438559c8dd71fc1cc60b6012a4d0f979e0b07e
Sergio Rodríguez Abreu <73601836+marmeymo@users.noreply.github.com>
2023-01-15 14:50:23 +0000

    Add files via upload

---
+++ Trimestre1/Tema3VScode/prueba_BBDD2/prueba_BBDD2/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace prueba_BBDD2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.conexion = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // conexion
+            // 
+            this.conexion.Location = new System.Drawing.Point(64, 70);
+            this.conexion.Name = "conexion";
+            this.conexion.Size = new System.Drawing.Size(101, 57);
+            this.conexion.TabIndex = 0;
+            this.conexion.Text = "Ver BBDD ";
+            this.conexion.UseVisualStyleBackColor = true;
+            this.conexion.Click += new System.EventHandler(this.conexion_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(232, 70);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(194, 263);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "BBDDs";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "DataGridView";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 383);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.conexion);
+            this.Name = "Form1";
+            this.Text = "Conexion Sencilla BBDD";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button conexion;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Trimestre1/Tema3VScode/prueba_BBDD2/prueba_BBDD2/Form1.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prueba_BBDD2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Parametros de la conexión
+        private static readonly String HOST = "localhost";
+        private static readonly String BD = "examen";
+        private static readonly String USER = "root";
+        private static readonly String PASSWORD = "";
+        private void conexion_Click(object sender, EventArgs e)
+        {
+            //Instancia de la clase MySqlConnection
+            MySqlConnection conexionBD = new MySqlConnection("Server=" + HOST + ";Database=" + BD + ";Uid=" + USER + ";Pwd=" + PASSWORD + ";SSL Mode=None");
+            try
+            {
+                //abrimos la conexión
+                conexionBD.Open();
+
+                //Objeto 'DataReader' de MySQL
+                MySqlDataReader reader = null;
+
+                //Objeto 'SqlCommand' de MySQL
+                MySqlCommand cmd = new MySqlCommand("SHOW DATABASES", conexionBD);
+                
+                //ejecutamos la consulta sobre la BBDD
+                reader = cmd.ExecuteReader();
+
+                //Leemos las lineas
+                while (reader.Read())
+                {
+                    //con reader.GetString(0) recogemos los valores de los registros
+                    Console.WriteLine(reader.GetString(0));
+
+                    //escribimos en listview
+                    ListViewItem lvi = new ListViewItem(reader.GetString(0));
+                    listView1.Items.Add(lvi);
+                }
+            }
+            catch
+            {
+                //Recogemos la excepción de Base de datos
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Users formUser = new Users();
+            formUser.Show();
+        }
+    }
+}
